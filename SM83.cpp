d2044eee80c9a5c4540d0dfa6ac2f5b598c99d32
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2025-06-16 23:26:23 -0400

    Cpu class added todo implementation

---
+++ SM83.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jas Sobolewski on 2025-06-16.
+//
+
+#include "SM83.h"
---
+++ SM83.h
@@ -0,0 +1,14 @@
+//
+// Created by Jas Sobolewski on 2025-06-16.
+//
+
+#ifndef GB_EMULATOR_SM83_H
+#define GB_EMULATOR_SM83_H
+
+
+class SM83 {
+
+};
+
+
+#endif //GB_EMULATOR_SM83_H
