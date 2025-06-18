0eb892c0e07d6c1078b7a054b36722462494aa4d
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2025-06-18 00:23:50 -0400

    TODO registers but progress made

--- SM83.cpp
+++ SM83.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "SM83.h"
+//TODO lamdas later in the morning
+void SM83::SetFlag(uint16_t value, uint8_t& flags) {
+    flags = value & 0xFF;
+}
+
+void SM83::SetAccumulator(uint16_t value, uint8_t& accumulator) {
+    accumulator = (value >> 8) & 0xFF;
+}
+
+void SM83::set_AF(uint16_t value) {
+    this->SetAccumulator(value, A);
+    this->SetFlag(value, F);
+    AF = ((A << 8)|F); // you put A and F into 16 bits you shift A to high postion
+}
+
+void SM83::set_BC(uint16_t value) {
+    this->SetAccumulator(value, B);
+    this->SetFlag(value, C);
+    BC = ((B << 8)|C); // you put A and F into 16 bits you shift A to high postion
+
+}
+
+void SM83::set_DE(uint16_t value) {
+
+}
+
+
+void SM83::set_HL(uint16_t value) {
+
+}
--- SM83.h
+++ SM83.h
@@ -6,7 +6,35 @@
 #define GB_EMULATOR_SM83_H
 
 
+#include <cstdint>
+
 class SM83 {
+    //Accumulators
+    uint8_t A,B,D,H;
+    //Flags
+    uint8_t F, C, E, L;
+
+    uint16_t SP; //Stack Pointer
+    uint16_t PC; //Program Counter
+
+    //Combined 16 bit registers
+    uint16_t AF, BC, DE,HL;
+
+    void SetFlag(uint16_t value, uint8_t& flags);
+
+    void SetAccumulator(uint16_t value, uint8_t& accumulator);
+
+    void set_AF(uint16_t value);
+
+    void set_BC(uint16_t value);
+
+    void set_DE(uint16_t value);
+
+    void set_HL(uint16_t value);
+
+
+
+
 
 };
 
